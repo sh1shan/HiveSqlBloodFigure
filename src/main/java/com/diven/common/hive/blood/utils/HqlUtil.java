5fd662c905126672fac74f320ca81c4c11aa3265
chenchaolin <578089476@qq.com>
2023-06-29 18:20:01 +0800

    1

--- src/main/java/com/diven/common/hive/blood/utils/HqlUtil.java
+++ src/main/java/com/diven/common/hive/blood/utils/HqlUtil.java
@@ -3,28 +3,34 @@ package com.diven.common.hive.blood.utils;
 import java.util.List;
 
 public class HqlUtil {
-	
-	public static String ListToString(List<String> hqls) {
-		StringBuffer buffer = new StringBuffer();
-		if(hqls != null) {
-			for(String sql : hqls) {
-				sql = sql.trim();
-				boolean flag = false;
-				for(String line : sql.split("\n")) {
-					if(line.trim().startsWith("--")) {
-						continue;
-					}
-					if(!line.trim().isEmpty()) {
-						flag = true;
-						buffer.append(line).append("\n");	
-					}
-				}
-				if(flag && !sql.endsWith(";")) {
-					buffer.append(";");
-				}
-			}
-		}
-		return buffer.toString();
-	}
-	
+
+    public static String ListToString(List<String> hqls) {
+        StringBuffer buffer = new StringBuffer();
+        if (hqls != null) {
+            for (String sql : hqls) {
+                //去除空格
+                sql = sql.trim();
+                //替换写在末尾的注释内容
+                sql = sql.replaceAll("--.*","");
+                // 替换多行注释
+                sql = sql.replaceAll("/\\*[\\s\\S]*?\\*/","");
+                boolean flag = false;
+                for (String line : sql.split("\n")) {
+                    //和正则的有重复，这是作者的，不影响程序执行
+                    if (line.trim().startsWith("--")) {
+                        continue;
+                    }
+                    if (!line.trim().isEmpty()) {
+                        flag = true;
+                        buffer.append(line).append("\n");
+                    }
+                }
+                if (flag && !sql.endsWith(";")) {
+                    buffer.append(";");
+                }
+            }
+        }
+        return buffer.toString();
+    }
+
 }
--- src/test/java/com/diven/common/hive/blood/figure/BaseTest.java
+++ src/test/java/com/diven/common/hive/blood/figure/BaseTest.java
@@ -2,21 +2,100 @@ package com.diven.common.hive.blood.figure;
 
 import com.alibaba.fastjson.JSON;
 import com.alibaba.fastjson.serializer.SerializerFeature;
-
 import junit.framework.TestCase;
 
 public class BaseTest extends TestCase{
 	
 	static String hqls [] = {
-			"create table temp.b1(id string, name string) row format delimited fields terminated by ',';",
-			"create table temp.b2(id string, age int) row format delimited fields terminated by ',';",
-			"create table temp.c1(id string, name string) row format delimited fields terminated by ',';",
-			"create table temp.c2(id string, age int) row format delimited fields terminated by ',';" ,
-			"create table temp.d1(id string, name string, age int) row format delimited fields terminated by ',';",
-			"from temp.a1 insert into table temp.b1 select id, name insert into table temp.b2 select id, age;",
-			"insert overwrite table temp.c1 select id, name from temp.b1;",
-			"insert overwrite table temp.c2 select id, age from temp.b2;" ,
-			"insert overwrite table temp.d1 select t1.id, t1.name, t2.age from temp.c1 t1 join temp.c2 t2 on t1.id = t2.id;"
+			"with tmp0 as (\n" +
+					"select\n" +
+					"    a.ds,\n" +
+					"    a.prod_id as sku_code,\n" +
+					"    a.sap_store_code,\n" +
+					"    a.stock_prod_qty_1d_ste,\n" +
+					"    a.stock_prod_qty_1d_stb,\n" +
+					"    sale_qty\n" +
+					"from miniso_ads.ads_scm_store_jxcrpt_1d a\n" +
+					"left join dim.dim_store_info_lastest t1 on a.sap_store_code=t1.sap_store_code\n" +
+					"where a.ds > date_sub('${ds}',28) and  a.ds <= '${ds}' and t1.sts_state = '3A'\n" +
+					"union all\n" +
+					"select\n" +
+					"        a.ds,\n" +
+					"        a.sku_code,\n" +
+					"        a.sap_store_code ,\n" +
+					"        stock_prod_qty_1d_ste ,\n" +
+					"        tock_prod_qty_1d_stb ,\n" +
+					"        a.sale_qty\n" +
+					"    from miniso_dws.dws_rpt_store_goods_sale_query_df a\n" +
+					"    left join dim.dim_store_info_lastest t1 on a.sap_store_code=t1.sap_store_code\n" +
+					"    where a.ds > date_sub('${ds}',28) and  a.ds <= '${ds}' and t1.sts_state = '3A'\n" +
+					")\n" +
+					",\n" +
+					"tmp as(\n" +
+					"    select\n" +
+					"           a.sku_code,\n" +
+					"           a.sap_store_code,\n" +
+					"           count(case when stock_prod_qty_1d_ste > 0 and stock_prod_qty_1d_stb > 0 and ds > date_sub('${ds}',7) then a.sap_store_code end ) as store_day_7,     --近7天符合条件门店数 --日均销门店数\n" +
+					"           count(case when stock_prod_qty_1d_ste > 0 and stock_prod_qty_1d_stb > 0 and ds > date_sub('${ds}',28) then a.sap_store_code end) as store_day_28,    --近28天符合条件门店数 --日均销门店数\n" +
+					"           sum(case when stock_prod_qty_1d_ste > 0 and stock_prod_qty_1d_stb > 0 and ds > date_sub('${ds}',7) then a.sale_qty else 0 end) as sale_qty_7,  --近七天符合条件销售数量  --日均销销量\n" +
+					"           sum(case when stock_prod_qty_1d_ste > 0 and stock_prod_qty_1d_stb > 0 and ds > date_sub('${ds}',28) then a.sale_qty else 0 end) as sale_qty_28  --近28天符合条件销售数量  --日均销销量\n" +
+					"    from (\n" +
+					"        select\n" +
+					"               ds,\n" +
+					"               sku_code,\n" +
+					"               sap_store_code,\n" +
+					"               sum(stock_prod_qty_1d_ste) as stock_prod_qty_1d_ste,\n" +
+					"               sum(stock_prod_qty_1d_stb) as stock_prod_qty_1d_stb,\n" +
+					"               sum(sale_qty) as sale_qty\n" +
+					"        from tmp0\n" +
+					"        group by ds,sku_code,sap_store_code\n" +
+					"    )a\n" +
+					"    group by a.sku_code,a.sap_store_code\n" +
+					")\n" +
+					",store_area as (\n" +
+					"select\n" +
+					"       a.sap_store_code,\n" +
+					"case when a.area_class1_name = '西藏区' then '西藏区' else b.area end as area\n" +
+					"from miniso_dim.dim_store_lastest  a\n" +
+					"left join miniso_ods.ods_youdata_442_miniso_sccm_sales_area b on a.comm_opt_manager = b.manager\n" +
+					"where a.area_class1_name in ('西藏区','华东经营大区','华北经营大区','华西经营大区','华南经营大区','华中经营大区')\n" +
+					")\n" +
+					"\n" +
+					"insert overwrite table miniso_ads.ads_ds_miniso_sccm_delivery_goods_store_detail partition(ds)\n" +
+					"select\n" +
+					"    a.stat_date                -- '统计日期'\n" +
+					"   ,a.wrh_code                 -- '仓库代码'\n" +
+					"   ,a.wrh_name                 -- '仓库名称'\n" +
+					"   ,'C1' as cate1_code         --'大类2代码'\n" +
+					"   ,t.cate1_name               -- '大类2名称'\n" +
+					"   ,t.cate_code                -- '细类代码'\n" +
+					"   ,t.cate_name                -- '细类名称'\n" +
+					"   ,a.sku_code                 -- '商品代码'\n" +
+					"   ,t.sku_name                --'商品名称'\n" +
+					"   ,t.pkg_price                -- '组合价'\n" +
+					"   ,a.store_code               -- '门店代码'\n" +
+					"   ,a.store_name               -- '门店名称'\n" +
+					"   ,d.area  --运营区域\n" +
+					"\n" +
+					"   ,(c.sale_qty_7/c.store_day_7)  as avg_sales_qty_7days      -- '单店日均销量（7天）'\n" +
+					"   ,(c.sale_qty_28/c.store_day_28)  as avg_sales_qty_28days     -- '单店日均销量（28天）'\n" +
+					"\n" +
+					"   ,b.hav_stock_store_qty      -- '当前有库存门店数'\n" +
+					"   ,b.hav_sales_rate_7days     -- '7天动销率'\n" +
+					"   ,b.being_wrh_turnover_days   -- '即将入仓周转'\n" +
+					"   ,b.wrh_turnover_days        -- '仓库周转'\n" +
+					"   ,a.zh_days                  -- '门店周转'\n" +
+					"\n" +
+					"   ,c.sale_qty_7 as sales_qty_7days          -- '销售量-7天（总）'\n" +
+					"   ,c.sale_qty_28 as sales_qty_28days         -- '销售量-28天（总）'\n" +
+					"   ,date_format(current_timestamp(), 'yyyy-MM-dd HH:mm:ss')\n" +
+					"   ,a.ds\n" +
+					"from miniso_ads.tmp_05_ads_ds_miniso_sccm_goods_wrh_detail_mid a\n" +
+					"left join miniso_ads.ads_ds_miniso_sccm_goods_wrh_detail_mid b on a.wrh_code = b.wrh_code and a.sku_code = b.sku_code and b.ds ='${ds}'\n" +
+					"left join tmp c on a.sku_code = c.sku_code and a.store_code = c.sap_store_code\n" +
+					"left join store_area d on a.store_code = d.sap_store_code\n" +
+					"left join miniso_ads.tmp_01_ads_ds_miniso_sccm_goods_detail_mid t on a.sku_code = t.sku_code and t.ds ='${ds}'\n" +
+					"where a.ds ='${ds}'\n"
 	};
 	
 
